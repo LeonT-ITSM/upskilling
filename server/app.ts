ba66f1c2c832bbeb441af26539694ad8e95c0d27
LeonT-ITSM <leon.townsend@digital.mod.uk>
2026-07-06 12:30:50 +0100

    initial commit

---
+++ server/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import nunjucks from "nunjucks";
+import morgan from "morgan";
+import path from "path";
+import defaultRouter from "./routes/default";
+
+const app = express();
+
+nunjucks.configure(["templates", "node_modules/govuk-frontend/dist"], {
+  autoescape: true,
+  express: app,
+});
+
+app.set("view engine", "njk");
+
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "../node_modules/govuk-frontend/dist/govuk/assets")),
+  express.static(path.join(__dirname, "../public"))
+);
+
+app.use(morgan("tiny"));
+app.use("/", defaultRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
